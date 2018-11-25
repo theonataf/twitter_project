3a457eaa3333e8e23fb9d77fd43fc7d9ce838882
Theo Nataf <theonataf@MacBook-Pro-de-Theo-2.local>
2018-11-25 10:57:02 +0200

    making some changes yoyo

--- twitter_project/first_app/forms.py
+++ twitter_project/first_app/forms.py
@@ -6,11 +6,16 @@ from django.contrib.auth.models import User
 class NewUserTweetForm(forms.Form):
 	text = forms.CharField(max_length=140, widget=forms.Textarea)
 	
+
+
+	
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
 	class Meta():
 		model = User
 		fields = ('username', 'email', 'password')
+
+
 
 
 class UserProfileInfoForm(forms.ModelForm):
